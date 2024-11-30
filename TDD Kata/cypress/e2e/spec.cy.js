415a74d52733a92bc521e06f3bb4651471b90583
Vishal <vibrus77@gmail.com>
2024-11-30 14:59:39 +0530

    feat-3: delimiter support added

--- TDD Kata/cypress/e2e/spec.cy.js
+++ TDD Kata/cypress/e2e/spec.cy.js
@@ -6,7 +6,7 @@ describe('provided input', () => {
 })
 
 describe('calculate with single number input', () => {
-	it('should check if calculated value is check - single number', () => {
+	it('should check calculated value - single number', () => {
   		cy.visit('http://localhost:5173/');
   		cy.get('textarea[name="input"]').type('1');
   		cy.get('form').submit();
@@ -15,7 +15,7 @@ describe('calculate with single number input', () => {
 })
 
 describe('calculate with single number input', () => {
-	it('should check if calculated value is check - single number', () => {
+	it('should check calculated value - single number', () => {
   		cy.visit('http://localhost:5173/');
   		cy.get('textarea[name="input"]').type('a');
   		cy.get('form').submit();
@@ -24,7 +24,7 @@ describe('calculate with single number input', () => {
 })
 
 describe('calculate with 2 inputs', () => {
-	it('should check if calculated value is check - two numbers, comma separated', () => {
+	it('should check calculated value - two numbers, comma separated', () => {
   		cy.visit('http://localhost:5173/');
   		cy.get('textarea[name="input"]').type('1,3');
   		cy.get('form').submit();
@@ -33,7 +33,7 @@ describe('calculate with 2 inputs', () => {
 })
 
 describe('calculate with 2 inputs', () => {
-	it('should check if calculated value is check - two numbers, new line separated', () => {
+	it('should check calculated value - two numbers, new line separated', () => {
   		cy.visit('http://localhost:5173/');
   		cy.get('textarea[name="input"]').type("1\n3");
   		cy.get('form').submit();
@@ -42,10 +42,19 @@ describe('calculate with 2 inputs', () => {
 })
 
 describe('calculate with multi inputs', () => {
-	it('should check if calculated value is check - two numbers, new line separated', () => {
+	it('should check calculated value - two numbers, new line separated, and comma separated', () => {
   		cy.visit('http://localhost:5173/');
   		cy.get('textarea[name="input"]').type("1\n2,3");
   		cy.get('form').submit();
 		cy.get('p.result').should('have.text', '6');
+	})
+})
+
+describe('Delimiter support', () => {
+	it('should check calculated value - delimiter on first line', () => {
+  		cy.visit('http://localhost:5173/');
+  		cy.get('textarea[name="input"]').type("//;\n1;2");
+  		cy.get('form').submit();
+		cy.get('p.result').should('have.text', '3');
 	})
 })
--- TDD Kata/src/App.jsx
+++ TDD Kata/src/App.jsx
@@ -3,17 +3,26 @@ import { useState, useEffect } from "react";
 
 function App() {
 	const [input, setInput] = useState('');
+	const [delimiter, setDelimiter] = useState('');
 	const [result, setResult] = useState(0);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		let sum = +result;
+		const regex = /\/\/(.*?)(\W)/;
+
 		if(input.length) {
-			if(input.includes(',') || input.includes('\n')) {
+			const match = input.match(regex);
+			if(match) {
+				setDelimiter(match[2]);
+			} else {
+				setDelimiter(',');
+			}
+			if(input.includes(delimiter) || input.includes('\n')) {
 				const numbersWithComma = input.split('\n');
 				numbersWithComma.forEach(num => {
-					if(num.includes(',')) {
-						sum += num.split(',')
+					if(num.includes(delimiter)) {
+						sum += num.split(delimiter)
 						.map((n) => parseFloat(n.trim()))
 						.filter((n) => !isNaN(n))
 						.reduce((acc, n) => acc+n, 0);
